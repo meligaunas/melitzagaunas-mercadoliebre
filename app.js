128ee08d99e9ca7454f7babbd368ab7a7d05ae0b
meligaunas <gaunasmelitza@gmail.com>
2023-07-28 10:44:37 -0300

    Login y register

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+
 
 
 app.listen(PORT,() => console.log('server running in http://localhost:' + PORT));
